a647b9167c72e0605f0e5a2723556cf0ef59e60e
Vladimir Grinenko <i@tadatuta.com>
2017-03-06 01:10:45 +0300

    initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# bem-font-awesome
+
+BEM port of [Font Awesome](http://fontawesome.io).
+
+## Usage
+
+1. Add the library to project dependencies:
+    ```
+    npm i bem-font-awesome --save
+    ```
+2. Add it as [redefinition level](https://en.bem.info/methodology/key-concepts/#redefinition-level)
+
+You're done :)
---
+++ generate.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const mv = require('mv');
+
+const faSourceFolder = path.join('node_modules', 'font-awesome');
+
+mv(path.join(faSourceFolder, 'fonts'), path.join(__dirname, 'fonts'), err => {
+    if (err.code !== 'ENOENT') console.error(err);
+});
+
+const selectorRegexp = /\.fa\-(.+)\:before/; // .fa-clock-o:before
+
+const css = fs.readFileSync('node_modules/font-awesome/css/font-awesome.css', 'utf8');
+
+const fa = postcss.plugin('fa', function(options = {}) {
+    return function(css) {
+        css.walkRules(function(rule) {
+            const parsedSelector = selectorRegexp.exec(rule.selector);
+            if (!parsedSelector) return;
+
+            const modVal = parsedSelector[1];
+
+            const result = [
+                '.fa_icon_' + modVal + ':before {'
+            ];
+
+            rule.walkDecls(function(decl, i) {
+                result.push(decl.raws.before + '  ' + decl.prop + decl.raws.between + decl.value + ';');
+            });
+
+            result.push('\n}');
+
+            fs.writeFileSync(path.join('fa', '_icon', 'fa_icon_' + modVal + '.css'), result.join(''));
+        });
+    }
+});
+
+postcss([fa]).process(css).then(result => console.log('done'));
